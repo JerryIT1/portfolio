b1aa9b35e49203f8460a989281a8521a577bf3b0
JollyPart-Desk-Main <94416528+JerryIT1@users.noreply.github.com>
2023-06-11 06:54:11 -0500

    Changed Resume
    Removed white space at bottom of page
    Moved <Resume /> out of header div

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -18,26 +18,27 @@ class ResumeIn extends React.Component {
   render() {
     return (
         <>
-        <div className="buttons__resume">
-            <button
-              className="resume__button"
-              type="button"
-              onClick={this.handleClick}
-            >
-              { this.state.show ? 'Hide' : 'Show' } Resume
-            </button>
+           <div className="buttons__resume">
+               <button
+                 className="resume__button"
+                 type="button"
+                 onClick={this.handleClick}
+               >
+                 { this.state.show ? 'Hide' : 'Show' } Resume
+               </button>
 
-            <button onClick={() => openInNewTab('https://drive.google.com/file/d/1k5bLgSyfI9pPOnpZ5fqM_NRZd5Nnk28F/view?usp=drive_link')} className="resume__button">
-                Open in New Tab
-            </button>
+               <button onClick={() => openInNewTab('https://drive.google.com/file/d/1k5bLgSyfI9pPOnpZ5fqM_NRZd5Nnk28F/view?usp=drive_link')} className="resume__button">
+                   Open in New Tab
+               </button>
+          </div>
+
+        <div className="resume-folder">
+          <Fade left opposite collapse when={this.state.show}>
+            
+              <img src="https://i.ibb.co/Kj9dp9Z/Berman-Nowlin-1-1.png" className={this.state.show ? "Resume-img" : "Resume-img-hidden"}/>
+          </Fade>
+
         </div>
-        
-      <div className="resume-folder">
-        <Fade left opposite collapse when={this.state.show}>
-            <img src="https://i.ibb.co/Kj9dp9Z/Berman-Nowlin-1-1.png" className="Resume-img"/>
-        </Fade>
-        
-      </div>
       </>
     );
   }
@@ -48,9 +49,9 @@ const Resume = () => {
         <div className="resume__container" id="resume">
             <div className="resume__container-header">
                 <h1>Resume</h1>
-                <ResumeIn />
+                
             </div>
-            
+            <ResumeIn />
         </div>
     )
 }
