191c81a0695e662007da6e39eb05991f27b2f73f
JollyPart <94416528+JerryIT1@users.noreply.github.com>
2023-06-12 02:53:58 -0500

    Changed Projects
    Made projects look a lot better

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,6 +7,11 @@ const Indiv1 = (props) => {
         <>
             <div className="projects__container-card">
                 <div className="projects__container-information">
+                    <Fade right>
+                    <div className="projects__container-card-picture">
+                        <img className="picture1" src={props.img}/>
+                    </div>
+                    </Fade>
                     <Fade left>
                     <div className="projects__container-card-title">
                         <h2>{props.name}</h2>
@@ -27,11 +32,7 @@ const Indiv1 = (props) => {
                     </div>
                     </Fade>
                 </div>
-                <Fade right>
-                <div className="projects__container-card-picture">
-                    <img className="picture1" src={props.img}/>
-                </div>
-                </Fade>
+
 
             </div>
         </>
@@ -47,21 +48,23 @@ const Projects = () => {
                 <h1>Projects</h1>
             </div>
             </Fade>
-            <Indiv1 
-                name={"MoviesMoviesMovies"} 
-                desc={"Created this to use the IMDB api and get updated information on all of the movies/shows in their database."}
-                github={'https://github.com/JerryIT1/MoviesWebsite'}
-                img={'https://i.ibb.co/RgXqbVH/image-2023-06-09-173604458.png'}
-            />
-            <Indiv1 
-                name={"Portfolio Website"}
-                desc={"This was my first website I created 100% on my own using react. You are looking at it right now!"}
-                github={"https://github.com/JerryIT1/portfolio"}
-                img={'https://i.ibb.co/tmYJs62/image-2023-06-10-002249356.png'}
+            <div className="projects__contianer-holder">
+                <Indiv1 
+                    name={"Movies Website"} 
+                    desc={"Created this to use the IMDB api and get updated information on all of the movies in their database."}
+                    github={'https://github.com/JerryIT1/MoviesWebsite'}
+                    img={'https://i.ibb.co/RgXqbVH/image-2023-06-09-173604458.png'}
+                />
+                <Indiv1 
+                    name={"Portfolio Website"}
+                    desc={"This was my first website I created 100% on my own using react. You are looking at it right now!"}
+                    github={"https://github.com/JerryIT1/portfolio"}
+                    img={'https://i.ibb.co/tmYJs62/image-2023-06-10-002249356.png'}
             
             
-            />
+                />
 
+            </div>  
         </div>
     )
 };
